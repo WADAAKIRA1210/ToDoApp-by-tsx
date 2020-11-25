3f7c15b3e333d4de3abbd8109d2d883089925908
Keisuke Wada <wadakei1210@gmail.com>
2020-11-25 23:01:33 +0900

    add compornents and modify index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,62 @@
 import * as ReactDOM from "react-dom";
 import { hot } from "react-hot-loader/root";
 import React, { useState } from "react";
-// import { InputForm } from "./compornents/InputForm";
+import { TextInput } from "./compornents/TextInput";
+import { SubmitButton } from "./compornents/SubmitButton";
 
-interface DisplayRegisteredTaskFunc {
-  (taskList: Array<string>): Array<string>;
-}
+type State = {
+  taskState: string;
+};
+
+type Action = { type: "WILL" } | { type: "DOING" } | { type: "DONE" };
+
+const initialState = {
+  taskState: "WILL",
+};
+
+const taskReducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case "WILL":
+      return {
+        ...state,
+        taskState: 0,
+      };
+    case "DOING":
+      return {
+        ...state,
+        taskState: 1,
+      };
+    case "DONE":
+      return {
+        ...state,
+        taskState: -1,
+      };
+    default:
+      return state;
+  }
+};
 
 const App = () => {
-  const [taskName, setTaskName] = useState("");
-  const [taskState, setTaskState] = useState("will");
+  const [taskName, setInputTask] = useState("");
+  const [TaskState, setNewTask] = useState("Will");
+
+  const handleInputValue = (value: string) => {
+    setInputTask(value);
+    console.log(value);
+  };
+  const submit = () => {
+    setNewTask("Will");
+  };
+
   return (
     <form>
-      <input
-        type="text"
-        value={taskName}
-        onChange={(evt) => setTaskName(evt.target.value)}
+      <TextInput
+        title="TaskName"
+        inputValue={taskName}
+        onChangeValue={handleInputValue}
       />
-      <button onClick={() => setTaskName((taskName) => (taskName = taskName))}>
-        Regist{taskName}
-      </button>
-      <br />
-      <div>
-        <button
-          onClick={() => setTaskState((prevState) => (prevState = "finish"))}
-        >
-          State( {taskState} )
-        </button>
-        {/* <InputForm /> */}
-        {/* <registTask /> */}
-      </div>
+      <SubmitButton title="Regist" onClick={submit} />
+      <div>{taskName}</div>
     </form>
   );
 };
